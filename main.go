2b6f9e6a0067bbac781404f74a4277b05c608d28
valkyrie_pilot <valk@randomairborne.dev>
2022-05-12 16:36:30 +0000

    Switch to go to prevent 1 hour builds

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("paste.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(file)
+	contents := buf.String()
+	tmpl, err := template.New("paste").Parse(contents)
+
+	file, err = os.Open("404.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	buf = new(bytes.Buffer)
+	buf.ReadFrom(file)
+	notfound := buf.String()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		req, err := http.Get("https://cdn.discordapp.com/attachments" + r.URL.Path)
+		if err != nil {
+			log.Println(err)
+			fmt.Fprintln(w, "An error occured fetching from the discord api")
+			return
+		}
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		defer req.Body.Close()
+		if req.StatusCode != 200 {
+			fmt.Fprintf(w, notfound)
+			return
+		}
+		contents, err := io.ReadAll(req.Body)
+		if err != nil {
+			log.Println(err)
+			fmt.Fprintln(w, "An error occured reading the contents of the response")
+			return
+		}
+		err = tmpl.Execute(w, string(contents))
+		if err != nil {
+			log.Println(err)
+			fmt.Fprintln(w, "An error occured templating the response")
+			return
+		}
+	})
+	log.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
