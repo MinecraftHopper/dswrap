888a6a66c01ef81f30b82cd92fc9b02d1199698a
Joshua Taylor <lordralex@gmail.com>
2022-05-13 09:15:05 -0500

    Clean up code slightly

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 )
 
+var contentTemplate *template.Template
+
 func main() {
 	file, err := os.ReadFile("paste.html")
 	if err != nil {
@@ -17,42 +19,47 @@ func main() {
 	}
 
 	contents := string(file)
-	tmpl, err := template.New("paste").Parse(contents)
+	contentTemplate, err = template.New("paste").Parse(contents)
 
 	file, err = os.ReadFile("404.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	notfound := string(file)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		req, err := http.Get("https://cdn.discordapp.com/attachments" + r.URL.Path)
-		if err != nil {
-			log.Println(err)
-			fmt.Fprintln(w, "An error occured fetching from the discord api")
-			return
-		}
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		defer req.Body.Close()
-		if req.StatusCode != 200 {
-			fmt.Fprintf(w, notfound)
-			return
-		}
-		contents, err := io.ReadAll(req.Body)
-		if err != nil {
-			log.Println(err)
-			fmt.Fprintln(w, "An error occured reading the contents of the response")
-			return
-		}
-		escaped := template.HTMLEscapeString(string(contents))
-		corrected := strings.Replace(escaped, "\n", "<br>", -1)
-		tmpl.Execute(w, template.HTML(corrected))
-		if err != nil {
-			log.Println(err)
-			fmt.Fprintln(w, "An error occured templating the response")
-			return
-		}
-	})
+	http.HandleFunc("/", handleRequest)
 	log.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	req, err := http.Get("https://cdn.discordapp.com/attachments" + r.URL.Path)
+	defer req.Body.Close()
+
+	if err != nil {
+		log.Println(err)
+		_, _ = fmt.Fprintln(w, "An error occurred fetching from the discord api")
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	if req.StatusCode != http.StatusOK {
+		_, _ = fmt.Fprintf(w, "File not found or discord error")
+		return
+	}
+	contents, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Println(err)
+		_, _ = fmt.Fprintln(w, "An error occurred reading the contents of the response")
+		return
+	}
+	escaped := template.HTMLEscapeString(string(contents))
+	corrected := strings.Replace(escaped, "\n", "<br>", -1)
+
+	err = contentTemplate.Execute(w, template.HTML(corrected))
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintln(w, "An error occurred templating the response")
+		return
+	}
 }
